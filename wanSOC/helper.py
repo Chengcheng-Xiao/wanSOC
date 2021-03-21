fa01d53ea5738f4231dfa4283bac04caec6bacfa
Chengcheng-Xiao <iconxicon@me.com>
2021-03-21 23:19:28 +0000

    fix compatiability problem with python2

--- wanSOC/helper.py
+++ wanSOC/helper.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 def printsocmat(orb,Msoc):
     """Print the soc matrix."""
     if orb=='p':
@@ -10,7 +12,7 @@ def printsocmat(orb,Msoc):
     for i in range(2*norbs):
         for j in range(2*norbs):
             if Msoc[i,j].real==0 and Msoc[i,j].imag==0:
-                print('%7.2f ' %0 , end='')
+                print('%7.2f ' %(0),end='')
             elif Msoc[i,j].real==0:
                 print ('%7.2fI' %(Msoc[i,j].imag),end='')
             elif Msoc[i,j].imag==0:
